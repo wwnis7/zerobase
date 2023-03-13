f62c57151068346ed9d4b0ce59af8912e1f562ca
minsoo Kim <46211067+wwnis7@users.noreply.github.com>
2023-03-13 21:23:39 +0900

    Update JavaStudy01.java

--- week01/JavaStudy01.java
+++ week01/JavaStudy01.java
@@ -1,8 +1,8 @@
 public class JavaStudy01 {
     public static void main(String[] args) {
         System.out.println("[구구단 출력]");
-        for (int i = 1; i < 10; i++) {
-            for (int j = 1; j < 10; j++) {
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
                 System.out.print(String.format("%02d x %02d = %02d", i, j, i*j));
                 System.out.print("\t");
             }
